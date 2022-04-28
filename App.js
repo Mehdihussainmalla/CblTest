f650833cb3e3807467d2250b156e1f91d9e1e0c8
CBL-Mac-037 <cbl-mac-037@CBL-Mac-037s-iMac-Pro.local>
2022-04-28 17:47:33 +0530

    again code push

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
 import Routes from './src/navigation/Routes';
-import store from './src/Redux/store';
+import store from './src/redux/store';
 import colors from './src/styles/colors';
  
 
 const App = () => {
+
+  useEffect(()=>{
+    
+
+
+  }), []
   return (
     <Provider store={store}>
     
