9418c46f88a990bf6950356e87355dff3812dad6
CBL-Mac-037 <cbl-mac-037@CBL-Mac-037s-iMac-Pro.local>
2022-05-09 17:13:07 +0530

    photo gallery start to upload

--- src/Screens/AddScreen/AddScreen.js
+++ src/Screens/AddScreen/AddScreen.js
@@ -1,19 +1,78 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, PermissionsAndroid, Platform, FlatList, Image } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import WrapperContainer from '../../Components/WrapperContainer'
-import navigationStrings from '../../navigation/navigationStrings'
+import CameraRoll from "@react-native-community/cameraroll";
+import Header from '../../Components/Header';
+import { moderateVerticalScale } from 'react-native-size-matters';
+import { height, width } from '../../styles/responsiveSize';
 
-const AddScreen = ({navigation}) => {
+const AddScreen = () => {
+
+  const [state, setState] = useState({
+    photos: '',
+  });
+
+  const androidPermission = async () => {
+
+    const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
+
+    const hasPermission = await PermissionsAndroid.check(permission);
+    if (hasPermission) {
+      return true;
+    }
+
+    const status = await PermissionsAndroid.request(permission);
+    return status === 'granted';
+  }
+
+  const addMedia = async () => {
+    if (Platform.OS === "android" && !(await androidPermission())) {
+      return;
+    }
+
+    CameraRoll.getPhotos({
+      first: 20,
+      assetType: 'Photos',
+    })
+      .then(res => {
+        setState({ photos: res.edges });
+      })
+      .catch((error) => {
+        console.log('error occurred at open gallery', error)
+      });
+  }
+  useEffect(() => {
+    addMedia();
+
+  }, [])
+  console.log("check add media ", addMedia)
+
   return (
- <WrapperContainer style={{backgroundColor:'red'}}>
+    <WrapperContainer>
+      <Header title={'Select photo'} />
+      <FlatList
+      
+        data={state.photos}
+        style={{ paddingBottom: moderateVerticalScale(80) }}
+        numColumns={3}
 
-      <TouchableOpacity onPress={()=>navigation.navigate(navigationStrings.POST_DETAIL)}
-       style={{backgroundColor:'white', justifyContent:'center',
-       alignItems:'center',
-       marginTop:30}}>
-      <Text style={{fontSize:25}}>AddScreen</Text>
-      </TouchableOpacity>
-      </WrapperContainer>
+        renderItem={(element, index) => {
+          return (
+            <>
+
+              <Image
+                key={index}
+                style={{ height: width / 3, width: width / 3 }}
+
+                source={{ uri: element.item.node.image.uri }}
+              />
+            </>
+          )
+        }}
+      />
+
+
+    </WrapperContainer>
   )
 }
 
--- src/Screens/PostAddScreen/PostAddScreen.js
+++ src/Screens/PostAddScreen/PostAddScreen.js
@@ -1,17 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View ,PermissionsAndroid, Platform,TouchableOpacity} from 'react-native'
 import React from 'react'
 import { styles } from './styles'
 import WrapperContainer from '../../Components/WrapperContainer'
 import Header from '../../Components/Header'
-// import CameraRoll from "@react-native-community/cameraroll";
+import CameraRoll from "@react-native-community/cameraroll";
+
 
 const PostAddScreen = () => {
+
+  const androidPermission = async ()=> {
+    const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
+    
+    const hasPermission = await PermissionsAndroid.check(permission);
+    if (hasPermission)
+    {
+      return true;
+    }
+
+    const status= await PermissionsAndroid.request(permission);
+    return status === 'granted';
+  }
+
+  const addMedia = async ()=>{
+    if (Platform.OS==="android" && ! (await androidPermission()))
+    {
+      return ;
+    }
+    CameraRoll.getPhotos({
+      first: 20,
+      assetType: 'Photos',
+    })
+    .then(res=> {
+      this.setState({ photos: res.edges });
+    })
+    .catch((error) => {
+      console.log('error occurred at open gallery',error)
+    });
+  }
+
+
+
+
+
+
   return (
   <WrapperContainer>
 
       <Header isBackIcon={true}
       title=' Add info'/>
-     <View style={{marginTop:10}}><Text>hello   </Text></View>
+     <TouchableOpacity style={{marginTop:10}}><Text>hello   </Text></TouchableOpacity>
       </WrapperContainer>
   )
 }
