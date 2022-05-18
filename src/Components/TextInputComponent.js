2493e54f4c6dd293e549342cbc29ece6d17014af
CBL-Mac-037 <cbl-mac-037@CBL-Mac-037s-iMac-Pro.local>
2022-05-18 18:51:55 +0530

    comment screen

--- src/Components/TextInputComponent.js
+++ src/Components/TextInputComponent.js
@@ -54,7 +54,8 @@ const styles = StyleSheet.create({
     },
     text: {
 
-
+color:colors.redB,
+fontSize:textScale(15)
 
 
     }
--- src/Screens/CommentScreen/CommentScreen.js
+++ src/Screens/CommentScreen/CommentScreen.js
@@ -1,33 +1,62 @@
 //import liraries
 import React from 'react';
-import { View, Text, StyleSheet,
-Image } from 'react-native';
+import {
+    View, Text, StyleSheet,
+    Image, ScrollView
+} from 'react-native';
 import Header from '../../Components/Header';
 import WrapperContainer from '../../Components/WrapperContainer';
 import imagePath from '../../constants/imagePath';
 import { height, textScale, width } from "../../styles/responsiveSize";
 import { moderateScale, moderateVerticalScale } from "react-native-size-matters";
+import colors from '../../styles/colors';
+import fontFamily from '../../styles/fontFamily';
+import ButtonComp from '../../Components/ButtonComp';
+import TextInputComponent from '../../Components/TextInputComponent';
 
 
-const CommentScreen = ({route}) => {
-    const item=(route?.params?.userData?.userData?.item)
-    const pic=(route?.params?.userData?.userData?.item?.user?.profile)
-    console.log("check profile>>>",item)
+const CommentScreen = ({ route }) => {
+    const item = (route?.params?.userData?.userData?.item)
+    const pic = (route?.params?.userData?.userData?.item?.user?.profile)
+    const profileName = (route?.params?.userData?.userData?.item?.user?.first_name)
+    const lastName = (route?.params?.userData?.userData?.item?.user?.last_name)
+    const email = (route?.params?.userData?.userData?.item?.user?.email)
+    // console.log("check profile>>>",item)
     return (
         <WrapperContainer>
-            <View style={{marginLeft:moderateVerticalScale(24),
-                 marginRight:moderateVerticalScale(23), backgroundColor:'grey'
-                 ,flex:1}}>
-            <Header
-                isBackIcon={true}
-                title={"Comments"} />
-            <View style={styles.container}>
-                <Image 
-                style={styles.iconstyle}
-                 source={{uri:pic}}
-                />
-                <Text style={styles.profile}>CommentScreen</Text>
-            </View>
+
+            <View style={{
+                marginLeft: moderateVerticalScale(24),
+                marginRight: moderateVerticalScale(23), backgroundColor: 'grey'
+                , flex: 1
+            }}>
+                <ScrollView>
+                    <Header
+                        isBackIcon={true}
+                        title={"Comments"} />
+                    <View style={styles.container}>
+                        <Image
+                            style={styles.iconstyle}
+                            source={{ uri: pic }}
+                        />
+                        <View style={{ flexDirection: 'column' }}>
+                            <Text style={styles.profile}>{profileName} <Text>{lastName}</Text></Text>
+                            <Text style={styles.email}>{email}</Text>
+                        </View>
+                    </View>
+                </ScrollView>
+
+                <View style={{
+                    flexDirection: 'row',
+                    backgroundColor: 'white',
+                }}>
+                    <TextInputComponent 
+                    input={{width:"120%"}}
+                    placeholder='Add a Comment' />
+                    <ButtonComp ButtonText='Post'
+                    buttonTxt={{width:'40%',marginLeft:25, }} />
+
+                </View>
             </View>
         </WrapperContainer>
     );
@@ -36,20 +65,27 @@ const CommentScreen = ({route}) => {
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection:'row',
-            },
-    iconstyle:{
+        flexDirection: 'row',
+    },
+    iconstyle: {
         marginTop: moderateVerticalScale(8),
         height: moderateScale(width / 10),
         width: moderateScale(width / 10),
         borderRadius: moderateScale(width / 20),
-        marginHorizontal:moderateScale(15)
+        marginHorizontal: moderateScale(15)
 
     },
-    profile:{
-        marginTop:moderateScale(20),
-        fontSize:textScale(13),
-        color:colors.white
+    profile: {
+        marginTop: moderateScale(10),
+        fontSize: textScale(13),
+        color: colors.white,
+        fontFamily: fontFamily.BarlowMedium
+    },
+    email: {
+        fontSize: textScale(13),
+        color: colors.white,
+        fontFamily: fontFamily.BarlowMedium
+
     }
 });
 
